4cd374c988e6176ff4abae5827e1e15177b50c29
Andrew Kim <aykim17@gmail.com>
2018-03-04 13:57:15 -0500

    Upload code for proof-of-concept

---
+++ FreeWSJ.js
@@ -0,0 +1,6 @@
+// FreeWSJ: Written by Andrew Kim
+
+var articleURL = window.location.href;
+
+// Articles can be read for free if you visit them through Facebook!
+open("https://www.facebook.com/flx/warn/?u=" + articleURL);
